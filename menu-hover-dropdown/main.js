43ebb6f73e9a115bf5e514ff067115334eea7e06
tykim <tykim1499@gmail.com>
2021-06-16 10:13:44 +0900

    add menu hover dropdown

---
+++ menu-hover-dropdown/main.js
@@ -0,0 +1,43 @@
+var header = document.querySelector(".page-header"),
+  mainMenuItems = document.querySelectorAll(".navbar-nav > li"),
+  subMenuItems = document.querySelectorAll(".navbar-nav .sub-nav"),
+  headerHeight = header.offsetHeight,
+  subMenuHeight = 0;
+
+// 그라디언트 헤더 배경
+header.style.backgroundImage =
+  "linear-gradient(" +
+  "white " +
+  headerHeight +
+  "px, rgba(255, 255, 255, 0.5) " +
+  headerHeight +
+  "px, rgba(255, 255, 255, 0.5))";
+
+for (var i = 0; i < mainMenuItems.length; i++) {
+  mainMenuItems[i].addEventListener("mouseover", function () {
+    if (this.getElementsByTagName("ul").length > 0) {
+      subMenuHeight = this.querySelector("ul").offsetHeight;
+      header.style.height = headerHeight + subMenuHeight + "px";
+    }
+  });
+  mainMenuItems[i].addEventListener("mouseout", function () {
+    header.style.height = headerHeight + "px";
+  });
+}
+
+// 전체 메뉴 높이 구하기
+// for (var i = 0; i < subMenuItems.length; i++) {
+//   if (subMenuItems[i].offsetHeight > subMenuHeight) {
+//     subMenuHeight = subMenuItems[i].offsetHeight;
+//   }
+// }
+// 헤더 높이값 주기
+// for (var i = 0; i < mainMenuItems.length; i++) {
+//   nav.addEventListener("mouseover", function () {
+//     //header.style.height = "270px";
+//     header.style.height = headerHeight + subMenuHeight + "px";
+//   });
+//   nav.addEventListener("mouseout", function () {
+//     header.style.height = headerHeight + "px";
+//   });
+// }
